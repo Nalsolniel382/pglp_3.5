4fd5000e945fcfee0a188def301c9d031c120776
Nalsolniel <thlapegue88@gmail.com>
2020-05-16 17:08:19 +0200

    une classemetier

--- exo3_5/src/main/java/fr/uvsq/exo3_5/UneClasseMetier.java
+++ exo3_5/src/main/java/fr/uvsq/exo3_5/UneClasseMetier.java
@@ -1,5 +1,23 @@
 package fr.uvsq.exo3_5;
 
 public class UneClasseMetier {
+	
+	
+	
+
+	private Affiche A;
+	
+	
+	public UneClasseMetier(final Affiche A) {
+		this.A = A;
+	}
+	
+	public void afficheTraitementMetier()
+	{
+		A.afficheDebutMetier();
+		// T r a i t em e n t s    mé t i e r s
+		A.afficheFinMetier();
+	}
+	
 
 }
