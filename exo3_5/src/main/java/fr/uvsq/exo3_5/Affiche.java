7290e6773ec7bf0c9ef445a0f3facfc73a131fae
Nalsolniel <thlapegue88@gmail.com>
2020-05-16 17:04:54 +0200

    construction de l affichage

--- exo3_5/src/main/java/fr/uvsq/exo3_5/Affiche.java
+++ exo3_5/src/main/java/fr/uvsq/exo3_5/Affiche.java
@@ -1,5 +1,21 @@
 package fr.uvsq.exo3_5;
 
-public class Affiche {
+
+import java.time.LocalDateTime;
+
+public class Affiche implements InterfaceAffiche {
+	
+	
+
+	public void afficheDebutMetier() {
+		
+		System.out.println(LocalDateTime.now() + "Début de afficheDebut");
+	}
+	
+	
+	public void afficheFinMetier() {
+		System.out.println(LocalDateTime.now() + "Début de afficheFin");
+	}
+
 
 }
--- exo3_5/src/main/java/fr/uvsq/exo3_5/InterfaceAffiche.java
+++ exo3_5/src/main/java/fr/uvsq/exo3_5/InterfaceAffiche.java
@@ -1,5 +1,12 @@
 package fr.uvsq.exo3_5;
 
 public interface InterfaceAffiche {
+	
+	
+
+	public void afficheDebutMetier();
+	
+	
+	public void afficheFinMetier();
 
 }
